c214adb6c51cb7062a15f8334ca5f98a39a9f772
Bryan Villatoro <bvillatoro@ymail.com>
2019-09-09 17:18:39 -0400

    get works first try

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -3,27 +3,22 @@ import BookApi from '../api/bookApi';
 import Dispatcher from '../dispatcher/appDispatcher';
 //Here add all crud actions for Books
 const BooksActions = {
-    bookList: [],
     readBooks: function () {
-        BookApi.getAllBooks().then((response) => {
-            this.bookList = response.data
-            //   console.log(this.bookList,"bookActions");
-        });
-        Dispatcher.dispatch({
-            actionType: 'read_books',
-            data: this.bookList
+        BookApi.getAllBooks().then(response => {
+            Dispatcher.dispatch({
+                actionType: 'read_books',
+                data: response.data
+            })
         });
     },
     addBook: function (book) {
         BookApi.addBook(book)
-        BookApi.getAllBooks().then((response) => {
-            this.bookList = response.data
+        BookApi.getAllBooks().then(response => {
+            Dispatcher.dispatch({
+                actionType: 'book_added',
+                data: response.data
+            });
         });
-        Dispatcher.dispatch({
-            actionType: 'book_added',
-            data: this.bookList
-        });
-
     }
 }
 

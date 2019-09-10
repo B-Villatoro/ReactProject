b37aee5ad07c10399f4dd7e7d2660897afd1e76f
Bryan Villatoro <bvillatoro@ymail.com>
2019-09-10 16:13:31 -0400

    add api working

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -12,10 +12,31 @@ const BooksActions = {
         });
     },
     addBook: function (book) {
-        BookApi.addBook(book)
+        BookApi.addBook(book);
+        BookApi.getAllBooks().then(response => {
+            Dispatcher.dispatch({
+                actionType: 'add_book',
+                data: response.data
+            })
+        });
     },
     deleteBook: (id) => {
         BookApi.deleteBook(id);
+        BookApi.getAllBooks().then(response => {
+            Dispatcher.dispatch({
+                actionType: 'delete_book',
+                data: response.data
+            })
+        });
+    },
+    updateBook: (book) => {
+        BookApi.updateBook(book);
+        BookApi.getAllBooks().then(response => {
+            Dispatcher.dispatch({
+                actionType: 'update_book',
+                data: response.data
+            })
+        });
     }
 }
 
